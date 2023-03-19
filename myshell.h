35793bc40734c05792e873a16bff934dd033c067
Conor Tilley <conor.tilley2@mail.dcu.ie>
2023-03-19 22:10:41 +0000

    Fishinshing touches

---
+++ myshell.h
@@ -0,0 +1,6 @@
+// Conor Tilley, 21307283
+// I acknowledge and understand the DCU Acedemic Integrity Policy
+
+int main(int argc, char **argv);
+void parse_input(char **args, char **arg, char *buff);
+void batchfile(char **argv, char **args, char *buff, char **arg);
